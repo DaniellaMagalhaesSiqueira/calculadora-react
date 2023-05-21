e14310b96459ce114bc9c652b90f147c66882d82
DaniellaMagalhaesSiqueira <daniella.siqueira80@gmail.com>
2023-05-21 12:00:23 -0300

    primeiro commit projeto calculadora curso coder

---
+++ src/main/Calculator.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import './Calculator.css'
+
+export default class Calculator extends Components{
+    render() {
+        return (
+            <div className='calculator'>
+                
+            </div>
+        )
+    }
+}
